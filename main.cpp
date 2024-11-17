93ffe4b194eca0bbfeb73e4eb5942d0964c2aee3
OtShelniko <100073633+OtShelniko@users.noreply.github.com>
2024-11-17 20:29:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,53 @@
-#include <iostream>
-#include "byblik.h"
+#include "modAlphakey.h"
+
+bool isValid(const std::wstring& s)
+{
+    for(auto c : s) {
+        if(!isalpha(c) || !isupper(c)) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main()
 {
-    std::string plaintext;
+    std::locale loc("ru_RU.UTF-8");
+    std::locale::global(loc);
     int key;
-    unsigned op;
+    std::wstring text;
+    int op;
 
-    std::cout << "Enter number of columns: ";
-    std::cin >> key;
-    std::cin.ignore();
-    byblik cipher(key);
+    std::wcout << L"Cipher ready. Input key: ";
+    std::wcin >> key;
 
+    if(!std::wcin.good()) {
+        std::wcerr << L"key not valid\n";
+        return 0;
+    }
+
+    modAlphakey cipher(key);
     do {
-        std::cout<<"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
-        std::cin>>op;
-        if (op > 2) {
-            std::cout<<"Illegal operation\n";
-        } else if (op >0) {
+        std::wcout << L"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
+        std::wcin >> op;
 
-            std::cout << "Enter plaintext: ";
-            std::cin.ignore();
-            std::getline(std::cin, plaintext);
+        if(op > 2) {
+            std::wcout << L"Illegal operation\n";
+        } else if(op > 0) {
+            std::wcout << L"Cipher ready. Input text: ";
+            std::wcin >> text;
 
-            if (op==1) {
-
-                std::string ciphertext = cipher.encrypt(plaintext);
-                std::cout << "Encrypted text: " << ciphertext << std::endl;
-
+            if(isValid(text)) {
+                if(op == 1) {
+                    std::wcout << L"Encrypted text: " << cipher.encrypt(text) << std::endl;
+                } else {
+                    std::wcout << L"Decrypted text: " << cipher.decrypt(text) << std::endl;
+                }
             } else {
-
-                std::string decryptedText = cipher.decrypt(plaintext);
-                std::cout << "Decrypted text: " << decryptedText << std::endl;
-
+                std::wcout << L"Operation aborted: invalid text\n";
             }
-
         }
-    } while (op!=0);
+    } while(op != 0);
+
     return 0;
 }
